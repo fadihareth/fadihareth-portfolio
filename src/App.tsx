1295d2bc0266ddd7f22646f359ed379d5e48ecf8
Fadi Hareth <fadihareth@gmail.com>
2025-07-14 16:37:53 -0400

    set font to Inter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
-        <div className="font-sans text-gray-800 scroll-smooth">
-            <main className="mt-16 space-y-24">
+        <div className="font-display scroll-smooth">
+            <main className="m-16 space-y-24">
             </main>
         </div>
     );
