6c995c649e636f2c5f2eb4edbdd909f453ad3560
Fadi Hareth <fadihareth@gmail.com>
2025-07-20 19:40:51 -0400

    adjust about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,14 @@
 export default function About() {
     return (
         <section id="about" className="space-y-4">
-            <h2 className="text-2xl font-bold">About</h2>
             <p>
                 I'm a software developer with a passion for building intuitive, elegant experiences — not just
                 in how users interact with an app, but also in how the codebase is structured underneath the hood.
-                I've worked across both mobile and web platforms, and enjoy the unique challenges and creative
-                opportunities each one brings. At its core, software development is simply something I love and
-                always have fun doing. Whether it's bringing ideas to life and solving problems through thoughtful
-                design and clean architecture or blending it with personal hobbies to create something personal,
-                I always take pride in my work and strive to keep improving.
+                I've worked across both mobile and web platforms, and enjoy the unique challenges and creativity
+                each one brings. At its core, software development is simply something I love and always have
+                fun doing. Whether it's bringing ideas to life and solving problems through thoughtful design
+                or blending it with personal hobbies to create something personal, I always take pride in my
+                work and strive to keep improving.
             </p>
         </section>
     );
