6e5e33b25e23689e87a75d90eb6d69bb970563c4
Fadi Hareth <fadihareth@gmail.com>
2025-07-21 14:29:25 -0400

    update footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="py-10 mx-16 max-lg:mx-8 text-center text-sm text-gray-500">
-            © {new Date().getFullYear()} Fadi Hareth. Built from scratch with Vite + React + TailwindCSS.
+            © {new Date().getFullYear()} Fadi Hareth. Built with Vite + React + TailwindCSS. Deployed with Netlify.
         </footer>
     );
 }
